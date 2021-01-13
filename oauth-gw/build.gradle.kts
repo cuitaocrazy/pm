30408e97291599991f5b25055b0dd3c9f4a74fc3
root <cuitaocrazy@163.com>
2021-01-13 13:54:43 +0800

    release 1.0.8

--- oauth-gw/build.gradle.kts
+++ oauth-gw/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.yada"
-version = "1.0.7"
+version = "1.0.8"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
   MAJOR=1
   MONOR=0
-  BUILD=7
+  BUILD=8

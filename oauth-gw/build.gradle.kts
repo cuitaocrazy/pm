a943d737d0de161f6e18a4ac1383bc907be50854
lirenhao <renhao.li@bjyada.com>
2021-02-04 09:20:32 +0800

    release 1.0.29

--- oauth-gw/build.gradle.kts
+++ oauth-gw/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.yada"
-version = "1.0.28"
+version = "1.0.29"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
   MAJOR=1
   MONOR=0
-  BUILD=28
+  BUILD=29

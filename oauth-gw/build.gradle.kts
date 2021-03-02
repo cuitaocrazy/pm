c7dffa0c3c3213bb060b44c7fc76674b20c0ef5c
cuitao <cuitao@cuitaodeMacBook-Pro.local>
2021-03-02 14:48:07 +0800

    升级spring

--- oauth-gw/build.gradle.kts
+++ oauth-gw/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.2"
+	id("org.springframework.boot") version "2.4.3"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.4.21"
-	kotlin("plugin.spring") version "1.4.21"
+	kotlin("jvm") version "1.4.30"
+	kotlin("plugin.spring") version "1.4.30"
 }
 
 group = "com.yada"
@@ -13,10 +13,9 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
 }
 
-extra["springCloudVersion"] = "2020.0.0"
+extra["springCloudVersion"] = "2020.0.1"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")

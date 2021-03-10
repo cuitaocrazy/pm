818ab21dc3ef13a5c268cf42cc79a16b1511f092
root <cuitaocrazy@163.com>
2021-03-10 10:21:14 +0800

    去除defaultValue警告

--- pm-web-app/src/pages/Proj/ProjForm.tsx
+++ pm-web-app/src/pages/Proj/ProjForm.tsx
@@ -75,7 +75,7 @@ export default (form: FormInstance<ProjectInput>, data?: ProjectInput) => {
         <InputNumber min={0} />
       </Form.Item>
       <Form.Item label="状态" name="status" rules={[{ required: true }]}>
-        <Select loading={loading} defaultValue={'onProj'}>
+        <Select loading={loading}>
           {projStatus.map((s) => (
             <Select.Option key={s[0]} value={s[0]}>
               {s[1]}

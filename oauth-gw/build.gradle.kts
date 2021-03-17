e321bdeeb36b01652c5d15264a51f8c53ede57a9
root <cuitaocrazy@163.com>
2021-03-17 09:58:33 +0800

    release 1.0.41

--- oauth-gw/build.gradle.kts
+++ oauth-gw/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.yada"
-version = "1.0.40"
+version = "1.0.41"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
   MAJOR=1
   MONOR=0
-  BUILD=40
+  BUILD=41

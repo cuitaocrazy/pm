33bcaf4f2cbe65906902eb58bbd917a2b3ea0f95
lirenhao <renhao.li@bjyada.com>
2021-03-18 15:28:38 +0800

    图表接口修改

--- pm-resource-server/src/graphql/resolvers/charts.ts
+++ pm-resource-server/src/graphql/resolvers/charts.ts
@@ -6,106 +6,106 @@ import { pool } from '../../postgres'
 async function getProjectManagerCharts (leader: string, year: number) {
   const projIds = await Project.find({ leader }).map(proj => proj._id).toArray()
 
-  const monthAmounts = await pool.query<{key: string; value: number}>(
+  const costOfMonths = await pool.query<{key: string; value: number}>(
     'SELECT yyyymm as key, SUM(amount)::NUMERIC as value FROM daily_settlement WHERE yyyy = $1 AND proj_id IN $2 GROUP BY yyyymm',
     [year, projIds],
   ).then(result => result.rows)
 
-  const monthCosts = await pool.query<{key: string; value: number}>(
+  const expenseOfMonths = await pool.query<{key: string; value: number}>(
     'SELECT yyyymm as key, SUM(amount)::NUMERIC as value FROM cost_settlement WHERE yyyy = $1 AND proj_id IN $2 GROUP BY yyyymm',
     [year, projIds],
   ).then(result => result.rows)
 
-  const monthMds = await pool.query<{key: string; value: number}>(
+  const mdOfMonths = await pool.query<{key: string; value: number}>(
     'SELECT yyyymm as key, SUM(md)::NUMERIC as value FROM daily_settlement WHERE yyyy = $1 AND proj_id IN $2 GROUP BY yyyymm',
     [year, projIds],
   ).then(result => result.rows)
 
-  const projCosts = await pool.query<{key: string; value: number}>(
+  const costOfProjs = await pool.query<{key: string; value: number}>(
     'SELECT proj_id as key, SUM(amount)::NUMERIC as value FROM daily_settlement WHERE yyyy = $1 AND proj_id IN $2 GROUP BY proj_id',
     [year, projIds],
   ).then(result => result.rows)
 
   return {
-    monthAmounts,
-    monthCosts,
-    monthMds,
-    projCosts,
-    empCosts: [],
-    groupCosts: [],
+    costOfMonths,
+    expenseOfMonths,
+    mdOfMonths,
+    costOfProjs,
+    costOfEmps: [],
+    costOfGroups: [],
   }
 }
 
 async function getGroupLeaderCharts (groups: string[], year: number) {
-  const monthAmounts = await pool.query<{key: string; value: number}>(
+  const costOfMonths = await pool.query<{key: string; value: number}>(
     'SELECT yyyymm as key, SUM(amount)::NUMERIC as value FROM daily_settlement WHERE yyyy = $1 AND emp_group IN $2 GROUP BY yyyymm',
     [year, groups],
   ).then(result => result.rows)
 
-  const monthCosts = await pool.query<{key: string; value: number}>(
+  const expenseOfMonths = await pool.query<{key: string; value: number}>(
     'SELECT yyyymm as key, SUM(amount)::NUMERIC as value FROM cost_settlement WHERE yyyy = $1 AND emp_group IN $2 GROUP BY yyyymm',
     [year, groups],
   ).then(result => result.rows)
 
-  const monthMds = await pool.query<{key: string; value: number}>(
+  const mdOfMonths = await pool.query<{key: string; value: number}>(
     'SELECT yyyymm as key, SUM(md)::NUMERIC as value FROM daily_settlement WHERE yyyy = $1 AND emp_group IN $2 GROUP BY yyyymm',
     [year, groups],
   ).then(result => result.rows)
 
-  const empCosts = await pool.query<{key: string; value: number}>(
+  const costOfEmps = await pool.query<{key: string; value: number}>(
     'SELECT emp_no as key, SUM(amount)::NUMERIC as value FROM daily_settlement WHERE yyyy = $1 AND emp_group IN $2 GROUP BY emp_no',
     [year, groups],
   ).then(result => result.rows)
 
   return {
-    monthAmounts,
-    monthCosts,
-    monthMds,
-    projCosts: [],
-    empCosts,
-    groupCosts: [],
+    costOfMonths,
+    expenseOfMonths,
+    mdOfMonths,
+    costOfProjs: [],
+    costOfEmps,
+    costOfGroups: [],
   }
 }
 
 async function getSupervisorCharts (year: number) {
-  const monthAmounts = await pool.query<{key: string; value: number}>(
+  const costOfMonths = await pool.query<{key: string; value: number}>(
     'SELECT yyyymm as key, SUM(amount)::NUMERIC as value FROM daily_settlement WHERE yyyy = $1 GROUP BY yyyymm',
     [year],
   ).then(result => result.rows)
 
-  const monthCosts = await pool.query<{key: string; value: number}>(
+  const expenseOfMonths = await pool.query<{key: string; value: number}>(
     'SELECT yyyymm as key, SUM(amount)::NUMERIC as value FROM cost_settlement WHERE yyyy = $1 GROUP BY yyyymm',
     [year],
   ).then(result => result.rows)
 
-  const monthMds = await pool.query<{key: string; value: number}>(
+  const mdOfMonths = await pool.query<{key: string; value: number}>(
     'SELECT yyyymm as key, SUM(md)::NUMERIC as value FROM daily_settlement WHERE yyyy = $1 GROUP BY yyyymm',
     [year],
   ).then(result => result.rows)
 
-  const groupCosts = await pool.query<{key: string; value: number}>(
+  const costOfGroups = await pool.query<{key: string; value: number}>(
     'SELECT emp_group as key, SUM(amount)::NUMERIC as value FROM daily_settlement WHERE yyyy = $1 GROUP BY emp_group',
     [year],
   ).then(result => result.rows)
 
   return {
-    monthAmounts,
-    monthCosts,
-    monthMds,
-    projCosts: [],
-    empCosts: [],
-    groupCosts,
+    costOfMonths,
+    expenseOfMonths,
+    mdOfMonths,
+    costOfProjs: [],
+    costOfEmps: [],
+    costOfGroups,
   }
 }
 
 function getDeafultCharts () {
   return ({
-    monthAmounts: [],
-    monthCosts: [],
-    monthMds: [],
-    projCosts: [],
-    empCosts: [],
-    groupCosts: [],
+    costOfMonths: [],
+    expenseOfMonths: [],
+    mdOfMonths: [],
+    costOfProjs: [],
+    costOfEmps: [],
+    costOfGroups: [],
   })
 }
 

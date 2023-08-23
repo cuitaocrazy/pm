614673373c36aca38505b18da5eaff31f2122133
邹海涛 <zouhaitao_wy@163.com>
2023-08-23 11:38:49 +0800

    测试部署，提交版本1.0.44

--- pm-resource-server/src/config/auth.ts
+++ pm-resource-server/src/config/auth.ts
@@ -1,4 +1,4 @@
 export default {
-  issuerBaseURL: process.env.ISSUER_BASE_URL || 'https://keycloak.yadadev.com/auth/realms/pm',
+  issuerBaseURL: process.env.ISSUER_BASE_URL || 'https://auth.yadashuke.com/auth/realms/pm',
   allowedAudiences: 'account',
 }

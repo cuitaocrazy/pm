5649fb5be699468e47623f78e1aa78f1e83a61b4
邹海涛 <zouhaitao_wy@163.com>
2023-08-30 16:35:47 +0800

    release 1.0.52

--- oauth-gw/build.gradle.kts
+++ oauth-gw/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.yada"
-version = "1.0.51"
+version = "1.0.52"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
   MAJOR=1
   MONOR=0
-  BUILD=51
+  BUILD=52

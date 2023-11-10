0cefc684c07df9fa1014e0811c322e2f7eb42d1e
邹海涛 <zouhaitao_wy@163.com>
2023-11-10 17:35:59 +0800

    release 1.0.78

--- oauth-gw/build.gradle.kts
+++ oauth-gw/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.yada"
-version = "1.0.77"
+version = "1.0.78"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
   MAJOR=1
   MONOR=0
-  BUILD=77
+  BUILD=78

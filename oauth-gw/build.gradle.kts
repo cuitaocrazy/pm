e7e7d311ffebb23e16e9bfef991b4a626486af47
ZreoDark <1161593628@qq.com>
2024-01-05 18:21:52 +0800

    release 1.0.111

--- oauth-gw/build.gradle.kts
+++ oauth-gw/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.yada"
-version = "1.0.110"
+version = "1.0.111"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
   MAJOR=1
   MONOR=0
-  BUILD=110
+  BUILD=111

f01d9f7d3212a5753be87466ef0ce14e4b56621d
zhmouohz <zhmouohz@163.com>
2024-02-04 15:18:15 +0800

    修复一个错误

--- pm-daily-notify/src/config/emailer.ts
+++ pm-daily-notify/src/config/emailer.ts
@@ -3,7 +3,11 @@ export default {
   port: process.env.EMAIL_PORT || 1025,
   user: process.env.EMAIL_USER || "project.1",
   password: process.env.EMAIL_PASSWORD || "secret.1",
+  // host: "smtp.263.net",
+  // port: 465,
+  // user: "miao.zhou@bjyada.com",
+  // password: "P@",
   subject: process.env.EMAIL_SUBJECT || "日报未提交通知",
   yanShouSubject: process.env.EMAIL_YANSHOU_SUBJECT || "验收时间提醒通知",
-  weiHuSubject: process.env.EMAIL_YANSHOU_SUBJECT || "维护时间提醒通知",
+  weiHuSubject: process.env.EMAIL_WEIHU_SUBJECT || "维护时间提醒通知",
 };

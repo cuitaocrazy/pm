e8e3456beae647204f340ef1c2499c2cd2a6b43f
13718154103 <1161593628@qq.com>
2025-02-28 15:28:31 +0800

    release 1.0.157

--- oauth-gw/build.gradle.kts
+++ oauth-gw/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 	kotlin("plugin.spring") version "1.4.30"       
 }       
 group = "com.yada"       
-version = "1.0.156"
+version = "1.0.157"
 java.sourceCompatibility = JavaVersion.VERSION_11       
 repositories {       
 	mavenCentral()       
